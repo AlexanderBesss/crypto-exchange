f628094cf8bfe5b55d2f8e318907e521d74e32b4
Oleksandr Besarab <alexandrbesss@gmail.com>
2025-09-04 10:31:21 +0300

    added unit tests for buy order

--- tests/unit/order-service.test.js
+++ tests/unit/order-service.test.js
@@ -1,12 +1,17 @@
 import { expect, jest } from '@jest/globals';
 import { OrderService } from "../../src/order/service/order-service.js";
 import { askOrder, bidOrder } from "./order-dataset.js";
+import { ConflictError, NotFoundError } from '../../src/core/response/http-response-type.js';
 
 describe('OrderService', ()=> {
     const orderRedisRepository = {
         findAllByBookId: jest.fn(),
+        findOrderById: jest.fn(),
+        deleteOrder: jest.fn(),
     };
-    const orderEventEmitter = {};
+    const orderEventEmitter = {
+        emit: jest.fn(),
+    };
     const orderService = new OrderService(orderRedisRepository, orderEventEmitter);
 
     describe('getOrdersByBook', () => {
@@ -17,6 +22,41 @@ describe('OrderService', ()=> {
             })
             const expectedOrders = await orderService.getOrdersByBook('bookId');
             expect(expectedOrders).toMatchObject({ask: [askOrderWithLowerPrice, askOrder ], bid: [bidOrder]});
+        });
+    });
+
+    describe('buyOrder', () => {
+        it('should throw NotFound error if order was not found', async ()=> {
+            jest.spyOn(orderRedisRepository, 'findOrderById').mockImplementation(() => undefined);
+            const expectedOrders = orderService.buyOrder('bookId');
+            await expect(expectedOrders).rejects.toThrow(NotFoundError);
+        });
+
+        it('should throw Conflict error if order has bid type', async ()=> {
+            jest.spyOn(orderRedisRepository, 'findOrderById').mockImplementation(() => bidOrder);
+            const expectedOrders = orderService.buyOrder('bookId');
+            await expect(expectedOrders).rejects.toThrow(ConflictError);
+        });
+
+        it('should buy an order', async ()=> {
+            jest.spyOn(orderRedisRepository, 'findOrderById').mockImplementation(() => askOrder);
+            const deleteSpy = jest.spyOn(orderRedisRepository, 'deleteOrder').mockImplementationOnce((bookId, orderId)=>{
+                expect(orderId).toEqual(askOrder.orderId);
+                expect(bookId).toEqual(askOrder.bookId);
+            });
+            const expectedOrder = {
+                ...askOrder,
+                status: 'closed',
+            }
+            const eventEmitterSpy = jest.spyOn(orderEventEmitter, 'emit').mockImplementationOnce((name, event)=>{
+                expect(event).toMatchObject(expectedOrder);
+            });
+
+            const result = await orderService.buyOrder('bookId');
+
+            expect(result).toMatchObject(expectedOrder);
+            expect(deleteSpy).toHaveBeenCalledTimes(1);
+            expect(eventEmitterSpy).toHaveBeenCalledTimes(1);
         });
     })
 });
